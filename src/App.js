b62a04dd427dc6238911493aff9f33c465b7d222
biraysutcuoglu <birays1999@gmail.com>
2022-05-19 12:10:52 +0100

    Async added with useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-  const cartIsShown = useSelector(state => state.ui.cartIsShown);
+  const cartIsShown = useSelector((state) => state.ui.cartIsShown);
+  //whenever there is a state change, send db. request
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://food-order-app-37cbd-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {cartIsShown && <Cart />}
